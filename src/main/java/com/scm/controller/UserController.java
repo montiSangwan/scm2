773b7efbb94a64674bf27af7576df64e0ba5b259
montiSangwan <montisangwan0403@gmail.com>
2024-08-29 07:40:24 +0530

    fetch username for logged in user

--- src/main/java/com/scm/controller/UserController.java
+++ src/main/java/com/scm/controller/UserController.java
@@ -1,20 +1,30 @@
 package com.scm.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.scm.helper.UsernameHelper;
+
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
-    
+
     @GetMapping("/dashboard")
     public String userDashboard() {
         return "user/dashboard";
     }
 
     @GetMapping("/profile")
-    public String userProfile() {
+    public String userProfile(Authentication authentication) {
+
+        String userName = UsernameHelper.getEmailOfLoggedInUser(authentication);
+        log.info(userName);
+        
         return "user/profile";
     }
 }
---
+++ src/main/java/com/scm/helper/UsernameHelper.java
@@ -0,0 +1,29 @@
+package com.scm.helper;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+
+public class UsernameHelper {
+    
+    public static String getEmailOfLoggedInUser(Authentication authentication) {
+
+        if (authentication instanceof OAuth2AuthenticationToken) {
+
+            OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) authentication;
+            String authorizedClientRegistrationId = token.getAuthorizedClientRegistrationId();
+
+            DefaultOAuth2User defaultOAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
+
+            if (authorizedClientRegistrationId.equalsIgnoreCase("google")) {
+                return defaultOAuth2User.getAttribute("name").toString();
+            } else {
+                return defaultOAuth2User.getAttribute("email") != null ? defaultOAuth2User.getAttribute("email").toString()
+                        : defaultOAuth2User.getAttribute("login").toString() + "@gmail.com";
+            }
+            
+        } else {
+            return authentication.getName();
+        }
+    }
+}
