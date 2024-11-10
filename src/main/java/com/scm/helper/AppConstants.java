b5ee36c15f6128d48acc329ff06a328dfb0aceec
montiSangwan <montisangwan0403@gmail.com>
2024-11-10 21:31:38 +0530

    Error resolved for empty profile pic while adding contact

--- src/main/java/com/scm/helper/AppConstants.java
+++ src/main/java/com/scm/helper/AppConstants.java
@@ -11,4 +11,6 @@ public class AppConstants {
     public static final Integer CONTACT_IMAGE_HEIGHT = 500;
 
     public static final String CONTACT_IMAGE_CROP = "fill";
+
+    public static final String DEFAULT_PROFILE_URL = "https://static-00.iconduck.com/assets.00/profile-default-icon-2048x2045-u3j7s5nj.png";
 }
--- src/main/java/com/scm/service/ImageServiceImpl.java
+++ src/main/java/com/scm/service/ImageServiceImpl.java
@@ -1,6 +1,7 @@
 package com.scm.service;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,7 +11,10 @@ import com.cloudinary.Transformation;
 import com.cloudinary.utils.ObjectUtils;
 import com.scm.helper.AppConstants;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class ImageServiceImpl implements ImageService {
 
     private final Cloudinary cloudinary;
@@ -21,23 +25,27 @@ public class ImageServiceImpl implements ImageService {
 
     @Override
     public String uploadImage(MultipartFile contactImage, String fileName) {
-        
+
         try {
-            byte[] data = new byte[contactImage.getInputStream().available()];
+            if (Objects.nonNull(contactImage) && contactImage.getInputStream().available() > 0) {
 
-            // update contact image content to byte array data
-            contactImage.getInputStream().read(data);
+                byte[] data = new byte[contactImage.getInputStream().available()];
 
-            // upload data to cloudinary
-            cloudinary.uploader().upload(data, ObjectUtils.asMap(
-                "public_id", fileName
-            ));
+                // update contact image content to byte array data
+                contactImage.getInputStream().read(data);
 
-            // return url of contact image to save in db
-            return this.getUrlFromPublicId(fileName);
+                // upload data to cloudinary
+                cloudinary.uploader().upload(data, ObjectUtils.asMap(
+                        "public_id", fileName));
+
+                // return url of contact image to save in db
+                return this.getUrlFromPublicId(fileName);
+            } else {
+                return AppConstants.DEFAULT_PROFILE_URL;
+            }
 
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("Error while uploading image to cloudinary", e.getMessage());
             return null;
         }
     }
@@ -45,14 +53,13 @@ public class ImageServiceImpl implements ImageService {
     @Override
     public String getUrlFromPublicId(String publicId) {
         return cloudinary
-        .url()
-        .transformation(
-            new Transformation<>()
-            .width(AppConstants.CONTACT_IMAGE_WIDTH)
-            .height(AppConstants.CONTACT_IMAGE_HEIGHT)
-            .crop(AppConstants.CONTACT_IMAGE_CROP)
-        )
-        .generate(publicId);
+                .url()
+                .transformation(
+                        new Transformation<>()
+                                .width(AppConstants.CONTACT_IMAGE_WIDTH)
+                                .height(AppConstants.CONTACT_IMAGE_HEIGHT)
+                                .crop(AppConstants.CONTACT_IMAGE_CROP))
+                .generate(publicId);
     }
-    
+
 }
