76ab2d93acc02e7af1e8c95adfbeee99cfcd8ebe
montiSangwan <montisangwan0403@gmail.com>
2024-12-05 21:31:25 +0530

    email testing

--- src/test/java/com/scmmonti/scm/ScmApplicationTests.java
+++ src/test/java/com/scmmonti/scm/ScmApplicationTests.java
@@ -1,13 +1,25 @@
 package com.scmmonti.scm;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.scm.service.EmailService;
 
 @SpringBootTest
 class ScmApplicationTests {
 
+	@Autowired
+	private EmailService emailService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void sendEmailTest() {
+		emailService.sendEmail("montisangwan1706@gmail.com",
+		 "this is testing", "this is scm project");
 	}
 
 }
