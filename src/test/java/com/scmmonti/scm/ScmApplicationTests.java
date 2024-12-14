4c0d0210d6ff7c3912f1becb312a23dc1f3c0e2c
montiSangwan <montisangwan0403@gmail.com>
2024-12-14 19:17:21 +0530

    deploy on aws server

--- src/test/java/com/scmmonti/scm/ScmApplicationTests.java
+++ src/test/java/com/scmmonti/scm/ScmApplicationTests.java
@@ -3,7 +3,9 @@ package com.scmmonti.scm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.scm.Application;
+
+@SpringBootTest(classes = Application.class)
 class ScmApplicationTests {
 
 	@Test
